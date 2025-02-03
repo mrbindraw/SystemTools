6a7a7baf981fd7a6c76368d2c10d6121cbc9bb17
Andrew Bindraw <mrbindraw@gmail.com>
2025-02-03 11:25:08 +0300

    Init commit

---
+++ Source/SystemTools/Private/SystemToolsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SystemToolsBPLibrary.h"
+#include "SystemTools.h"
+
+USystemToolsBPLibrary::USystemToolsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USystemToolsBPLibrary::SystemToolsSampleFunction(float Param)
+{
+	return -1;
+}
+
